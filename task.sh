ef424dfd2cc65f7e2a0331c83df5e576ac18413b
Andrii Shukalo <shukaload@gmail.com>
2026-03-31 01:05:17 +0300

    Create task.sh for cron backup

--- task.sh
+++ task.sh
@@ -1,1 +1,5 @@
 #! /bin/bash
+user=$DB_USER
+pass=$DB_PASSWORD
+mysqldump -u $user -p $pass --databases ShopDB  --result-file=ShopDBReserve
+mysqldump -u $user -p $pass --databases ShopDB --no-create-db   --result-file=ShopDBDevelopment
