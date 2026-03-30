876cbf46685b7e19a6e6407c772226b13e2d6233
Andrii Shukalo <shukaload@gmail.com>
2026-03-31 01:19:23 +0300

    Fix 1.0.0

--- task.sh
+++ task.sh
@@ -6,4 +6,4 @@ mysqldump -u $user -p"$pass" --databases ShopDB  --result-file=ShopDBBackup
 mysql -u $user -p"$pass" ShopDBReserve < ShopDBBackup
 mysqldump -u $user -p"$pass" --databases ShopDB --no-create-db --no-create-info --result-file=ShopDBDataBackup
 #mysql -u $user -p"$pass" -e "CREATE DATABASE ShopDBDevelopment;"
-mysql -u $user -p"$pass" ShopDBDevelopment  < ShopDBDevelopment
+mysql -u $user -p"$pass" ShopDBDevelopment  < ShopDBDataBackup
